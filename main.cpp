58f52f451728ae5b14250ea134f32a585402466e
Denis <ddd@ddd.com>
2026-02-10 18:28:12 +0300

    func: get max index

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
 int main() {
+	int array[10] = {4,7,1,9,90,23,12,89,12,-10};
+
+	int i_max = 0;
+	for (int i = 1; i < 10; i++) {
+		if (array[i] > array[i_max]) {
+			i_max = i;
+		}
+	}
+
+	std::cout << "index = " << i_max << " val = " << array[i_max] << std::endl;
+
+
+
 
 	return 0;
 }
