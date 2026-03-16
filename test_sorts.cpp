868cdab23cac806b4e18af6765fdaaa055613f83
Hellstaff218 <spitsynvadim7@gmail.com>
2026-03-16 19:33:51 +0300

    Реализована функция для проверки массива на отсортированность.

--- test_sorts.cpp
+++ test_sorts.cpp
@@ -1,37 +1,169 @@
 #include "test_sorts.h"
 #include "sorts.h"
 
+#pragma region Functions
 
-bool checkSort(const int* ar, int size) {
-	for (int i = 0; i < size - 1; i++) {
-		if (ar[i] > ar[i+1]) {
+bool checkSort(const int *ar, int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		if (ar[i] > ar[i + 1])
+		{
 			return false;
 		}
 	}
 	return true;
-
 }
 
-void test_selectionSort() {
+bool arraysEqual(const int *a, const int *b, unsigned int size)
+{
+	for (unsigned int i = 0; i < size; i++)
 	{
-		int ar[4] = {5,4,3,2};
-		selectionSort(ar, 4);
-		if (checkSort(ar, 4) == true) {
-			std::cout << "TEST PASSed" << std::endl;
-		}
-		else {
-			std::cout << "TEST FAIL" << std::endl;
+		if (a[i] != b[i])
+		{
+			return false;
 		}
 	}
-
-	{
-		int ar[4] = {5,4,3,2};
-		if (checkSort(ar, 4) == true) {
-			std::cout << "TEST PASSed" << std::endl;
-		}
-		else {
-			std::cout << "TEST FAIL" << std::endl;
-		}
-	}
-
+	return true;
 }
+
+void printArray(const int *ar, unsigned int size)
+{
+	for (unsigned int i = 0; i < size; i++)
+		std::cout << ar[i] << " ";
+
+	std::cout << std::endl;
+}
+
+void printTestResult(const char *name, bool ok)
+{
+	std::cout << (ok ? "[PASS]" : "[FAIL]") << name << std::endl;
+}
+
+void printTestResult(
+	const char *testName,
+	const int *expected,
+	const int *actual,
+	unsigned int size)
+{
+	if (arraysEqual(expected, actual, size))
+	{
+		std::cout << "[PASS] " << testName << std::endl;
+	}
+	else
+	{
+		std::cout << "[FAIL] " << testName << std::endl;
+
+		std::cout << "expected: ";
+		printArray(expected, size);
+
+		std::cout << "actual:   ";
+		printArray(actual, size);
+	}
+}
+
+#pragma endregion
+
+#pragma region Tests
+
+#pragma region isSorted
+
+void test_isSorted_ascending()
+{
+	int ar[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int res = isSorted(ar, 10);
+
+	if (res == 1)
+	{
+		std::cout << "[PASS] isSorted ascending" << std::endl;
+	}
+	else
+	{
+		std::cout << "[FAIL] isSorted ascending (got " << res << " expected 1)" << std::endl;
+	}
+}
+
+void test_isSorted_descending()
+{
+
+	int ar[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+	int res = isSorted(ar, 10);
+
+	if (res == -1)
+	{
+
+		std::cout << "[PASS] isSorted descending" << std::endl;
+	}
+	else
+	{
+		std::cout << "[FAIL] isSorted descending (got" << res << " expented -1)" << std::endl;
+	}
+}
+
+void test_isSorted_unsorted()
+{
+	int ar[] = {5, 4, 3, 1, 10, 13, 16, 2, 7, 29};
+	int res = isSorted(ar, 10);
+
+	if (res == 0)
+	{
+		std::cout << "[PASS] isSorted unsorted" << std::endl;
+	}
+	else
+	{
+		std::cout << "[FAIL] isSorted unsorted (got" << res << " expented 0)" << std::endl;
+	}
+}
+
+#pragma endregion
+
+#pragma region Selection sort
+
+void test_selectionSort_ascending()
+{
+	int ar[] = {5, 1, 4, 2, 8};
+	int expected[] = {1, 2, 4, 5, 8};
+
+	selectionSort(ar, 5, true);
+
+	printTestResult("selectionSort ascending", expected, ar, 5);
+}
+
+void test_selectionSort_descending()
+{
+	int ar[] = {5, 1, 4, 2, 8};
+	int expected[] = {8, 5, 4, 2, 1};
+
+	selectionSort(ar, 5, false);
+
+	printTestResult("selectionSort ascending", expected, ar, 5);
+}
+
+void test_selectionSort_sortedInput()
+{
+	int ar[] = {5, 1, 4, 2, 8};
+	int expected[] = {1, 2, 4, 5, 8};
+
+	selectionSort(ar, 5);
+	printTestResult("selectionSort sortedInput", expected, ar, 5);
+}
+
+#pragma endregion
+
+#pragma region Insertion sort
+
+void test_insertionSort_ascending()
+{
+}
+
+void test_insertionSort_descending()
+{
+}
+
+void test_insertionSort_sortedInput()
+{
+}
+
+#pragma endregion
+
+#pragma endregion
--- test_sorts.h
+++ test_sorts.h
@@ -3,8 +3,15 @@
 
 #include <iostream>
 
+void test_isSorted_ascending();
+void test_isSorted_descending();
+void test_isSorted_unsorted();
 
-void test_selectionSort();
+void test_selectionSort_ascending();
+void test_selectionSort_descending();
+void test_selectionSort_sortedInput();
 
+void test_insertionSort_ascending();
+void test_insertionSort_descending();
+void test_insertionSort_sortedInput();
 #endif
-
