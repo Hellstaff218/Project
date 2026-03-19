bc541f1b09d3f4b2730c5d0cc43428e382c4e945
Hellstaff218 <spitsynvadim7@gmail.com>
2026-03-19 20:20:55 +0300

    SW_2_1: добавлены скрипты, описание задания и реализовано первое задание

---
+++ setup_git.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Настройка Git..."
+
+git config --global user.name "RL6-21Ebyri"
+git config --global user.email "apodoprigorin85@gmail.com"
+
+echo "Готово. Текущие настройки:"
+git config --global --list
