1699469d7c354cfe971791c9a71c3bb2f089f279
Hellstaff218 <spitsynvadim7@gmail.com>
2026-04-07 15:26:51 +0300

    RK_1_1: модернизирована структура Box

--- box.h
+++ box.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <ostream>
+
 struct Box
 {
     // длина
@@ -10,7 +12,17 @@ struct Box
     int _height;
     // цвет
     int _color;
+    // объем
+    int volume;
+
+    Box();
+    Box(int value);
+    Box(int len, int width, int height);
+
+    void updateVolume();
 };
+
+std::ostream &operator<<(std::ostream &out, const Box &box);
 
 void writeToFile(const char *fileName, const Box &box);
 void readFromFile(const char *fileName, Box &box);
