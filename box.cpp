8196cfc0a4e02db399e7b5504643869826ac3406
RL6-21Ebyri <apodoprigorin85@gmail.com>
2026-04-07 15:26:51 +0300

    RK_1_1: модернизирована структура Box

--- box.cpp
+++ box.cpp
@@ -2,6 +2,48 @@
 
 #include <fstream>
 
+Box::Box()
+{
+    _len = 10;
+    _width = 10;
+    _height = 10;
+    _color = 0;
+    updateVolume();
+}
+
+Box::Box(int value)
+{
+    _len = value;
+    _width = value;
+    _height = value;
+    _color = 0;
+    updateVolume();
+}
+
+Box::Box(int len, int width, int height)
+{
+    _len = len;
+    _width = width;
+    _height = height;
+    _color = 0;
+    updateVolume();
+}
+
+void Box::updateVolume()
+{
+    volume = _len * _width * _height;
+}
+
+std::ostream &operator<<(std::ostream &out, const Box &box)
+{
+    out << std::dec
+        << "len = " << box._len
+        << ", width = " << box._width
+        << ", height = " << box._height
+        << ", volume = " << box.volume << '\n';
+    return out;
+}
+
 void writeToFile(const char *fileName, const Box &box)
 {
     if (fileName == nullptr)
@@ -31,6 +73,7 @@ void readFromFile(const char *fileName, Box &box)
     }
 
     in.read(reinterpret_cast<char *>(&box), sizeof(Box));
+    box.updateVolume();
 }
 void readFromFile(const char *fileName, Box *box)
 {
