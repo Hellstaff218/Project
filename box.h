21fb803a26347c3d7a7bf260a27d3db4bacbdc04
RL6-21Ebyri <apodoprigorin85@gmail.com>
2026-04-27 19:20:36 +0300

    SW_5_1: добавлены уникальные номера Box

--- box.h
+++ box.h
@@ -12,17 +12,25 @@ struct Box
     int _height;
     // цвет
     int _color;
+    // уникальный номер объекта
+    int _boxNumber;
     // объем
     int volume;
+    // флаг отрицательного объема
+    bool _hasNegativeVolume;
 
     Box();
     Box(int value);
     Box(int len, int width, int height);
+    Box(const Box &other);
+    Box &operator=(const Box &other);
+    ~Box();
 
     void updateVolume();
 };
 
 std::ostream &operator<<(std::ostream &out, const Box &box);
+void printBoxNumberState(std::ostream &out);
 
 void writeToFile(const char *fileName, const Box &box);
 void readFromFile(const char *fileName, Box &box);
