13a6ae95fb9376c31fc6d26881dbd3e543fac5f3
Hellstaff218 <spitsynvadim7@gmail.com>
2026-05-11 22:10:53 +0300

    Update bit_operations.cpp

--- bit_operations.cpp
+++ bit_operations.cpp
@@ -46,7 +46,43 @@ unsigned int x=number;
 	while (x!=0) {
 		count = count + (x&1);
 		x=x>>1;
-	}
+} 
 return count;
+}
 
+void cycleShiftArray(int* array, int size, int N, bool left)  {
+	N = N % 32;
+if (N == 0 || size <= 0) return;
+bool ok = true;
+if (left==0) ok = false;
+unsigned int temp1 = ok? (array[0]>>(32-N)): (array[size-1]<<(32-N));
+	if (ok) {
+		for (int i = 0; i < size - 1; i++) {
+		unsigned int temp2 = array[i + 1] >> (32 - N);
+		array[i] = (array[i] << N) | temp2;
+		}
+	array[size - 1] = (array[size - 1] << N) | temp1;
+	}
+	else {
+		for (int i =size-1; i>0;i--) {
+		unsigned int temp2 = array[i-1]<<(32-N);
+		array[i]=(array[i]>>N) | temp2;
+		}
+	array[0]=(array[0]>>N) | temp1;
+	}
+}
+
+void setBit(int* number, int numBit) { 
+int bit = 1;
+	bit =bit << numBit;
+
+ *number = *number | bit;
+
+
+}
+
+void clearBit(int* number, int numBit) {
+	int bit =1;
+	bit=bit << numBit;
+	*number=*number &(~bit);
 }
