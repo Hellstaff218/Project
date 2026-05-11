fb5e47e90e30b7a86dd766ae0226ee7388389995
Hellstaff218 <spitsynvadim7@gmail.com>
2026-05-11 22:03:57 +0300

    Create sorts.h

---
+++ sorts.h
@@ -0,0 +1,10 @@
+#ifndef _sorts_h_
+#define _sorts_h_
+
+#include <iostream>
+
+
+int getMax(const int*, int); 
+void selectionSort(int* ar, unsigned int size, bool ascending);
+int isSorted(const int* array, unsigned int size);
+#endif
