265f029ae5cb4b4c19ea694f2f1249d86e0613de
Hellstaff218 <spitsynvadim7@gmail.com>
2026-05-11 22:08:37 +0300

    Create file_operations.h

---
+++ file_operations.h
@@ -0,0 +1,9 @@
+#ifndef _file_operations_
+#define _file_operations_
+
+#include <iostream>
+
+void fillFile(const char* fileName, int min, int max, char delimeter, int cols, int
+countElements);
+
+#endif
